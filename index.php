9015a2dd6219bdfb66f9c17425f0de3fe65ba68c
dfcastro@outlook.com.br <dfcastro@outlook.com.br>
2020-11-10 16:50:47 -0300

    site produtos

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Slim\Slim;
 $app = new Slim();
 
 require_once("site.php");
+require_once("functions.php");
 require_once("admin.php");
 require_once("admin-users.php");
 require_once("admin-categories.php");
--- site.php
+++ site.php
@@ -1,11 +1,16 @@
 <?php 
 
-use \Ecommerce\PageAdmin;
+use \Ecommerce\Page;
+use \Ecommerce\Model\Product;
 
 $app->get('/', function() {
     
+    $products = Product::listAll();
+
 	$page = new Page();
-	$page->setTpl("index");
+
+	$page->setTpl("index",[
+		'products'=>Product::checkList($products)]);
 
 });
 
